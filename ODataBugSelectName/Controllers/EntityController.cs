2bc096325097e255f0a74e203216a6c4a6fd94db
Meikel Philipp <MeikelLP@users.noreply.github.com>
2021-09-17 22:53:27 +0200

    INITIAL COMMIT

---
+++ ODataBugSelectName/Controllers/EntityController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+
+namespace ODataBugSelectName.Controllers
+{
+    [ApiController]
+    [Route("/[controller]/[action]")]
+    public class EntityController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+        private readonly IMapper _mapper;
+
+        public EntityController(MyDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+
+            _db.Database.EnsureCreated();
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Get(ODataQueryOptions<MyResponse> options)
+        {
+            return Ok(await _db.Entities.GetQueryAsync(_mapper, options, null));
+        }
+    }
+
+    public class MyResponse
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+
+    public class MyProfile : Profile
+    {
+        public MyProfile()
+        {
+            CreateMap<MyEntity, MyResponse>();
+        }
+    }
+}
---
+++ ODataBugSelectName/MyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ODataBugSelectName
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<MyEntity> Entities { get; set; }
+
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MyEntity>().HasData(
+                new MyEntity { Id = Guid.NewGuid(), Name = "MyEntity 1", Number = 1},
+                new MyEntity { Id = Guid.NewGuid(), Name = "MyEntity 2", Number = 2}
+            );
+        }
+    }
+
+    public class MyEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
